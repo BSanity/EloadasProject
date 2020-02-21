12c1cb2ac84cdce2e7a72b23ce0037c824b6cffe
Tomi <eggcognito1337@gmail.com>
2020-02-21 12:58:44 +0100

    change5

--- EloadasProject/Class1.cs
+++ EloadasProject/Class1.cs
@@ -67,6 +67,32 @@ namespace EloadasProject
            
         }
 
+        public bool Teli()
+        {
+            bool telivane = true;
+            for (int i = 0; i < foglalasok.GetLength(0); i++)
+            {
+                for (int j = 0; j < foglalasok.GetLength(1); j++)
+                {
+                    if (foglalasok[i, j] == false)
+                    {
+                        telivane = false;
+                    }
+
+                }
+            }
+              return telivane;
+        }
+      
+        public bool Foglalt(int sorSzam, int helySzam)
+        {
+            if (sorSzam<0||helySzam<0)
+            {
+                throw new ArgumentException();
+            }
+            return foglalasok[sorSzam, helySzam];
+        }
+        
     }
-    
+
 }
