a00062db8f89a2407d8113ff3dbe9fc32ba64710
Tomi <eggcognito1337@gmail.com>
2020-02-21 12:22:22 +0100

    Change1

--- EloadasProject/Class1.cs
+++ EloadasProject/Class1.cs
@@ -5,24 +5,58 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace EloadasProject
-{  
-        
-      
+{
+
+
 
     public class Eloadas
     {
-        bool[,] foglalasok;
-
-        public Eloadas(int sorokSzama,int helyekSzama)
+        
+         bool[,] foglalasok;
+        public Eloadas(int sorokSzama, int helyekSzama)
         {
-            this.foglalasok = new bool[sorokSzama, helyekSzama];
 
-            if (!(sorokSzama >=0) || !(helyekSzama <= 0))
+            if (!(sorokSzama > 0) || !(helyekSzama > 0))
             {
-            throw new ArgumentException(String.Format("A paraméterek nem nagyobbak 0-nál {0}:"));
+                throw new ArgumentException(String.Format("A paraméterek nem nagyobbak 0-nál {0}:"));
 
-           }
+            }
+
+            this.foglalasok = new bool[sorokSzama, helyekSzama];
         }
+
+        public bool Lefoglal()
+        {
+
+            bool vege = false;
+            
+            for (int i = 0; i < foglalasok.GetLength(0); i++)
+                {
+                for (int j = 0; j < foglalasok.GetLength(1); j++)
+                {
+                  if (foglalasok [0,0]!=true && vege==false)
+                    {
+                        foglalasok[0, 0] = true;
+                            vege = true;
+
+                    }
+         
+                }
+
+            }
+
+
+
+            
+
+        
+
+          
+            
+        }
+           
+     
+
     }
     
 }
